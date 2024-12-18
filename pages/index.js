0d9a898a6975a5318635daca11592375713c7820
flo7000 <95176997+flo7000@users.noreply.github.com>
2024-12-18 13:11:18 +0100

    Update index.js - added icon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,8 @@ export default function Home() {
         <title>home | miamibeachhotel</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/png" href="/icon.png" />
+        {/* Updated favicon link */}
+        <link rel="icon" type="image/png" href="https://theblackoutszn.miamibeachhotel.li/icon.png" />
         <link rel="stylesheet" type="text/css" href="/styles.css" />
       </Head>
 
