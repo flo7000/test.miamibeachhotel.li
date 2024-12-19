05f49ec4166bd93afca12e40c3551c4c0a962004
flo7000 <95176997+flo7000@users.noreply.github.com>
2024-12-19 17:06:43 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'media.test.miamibeachhotel.li'
-            }
-          ],
-          destination: '/media/:path*'
-        }
-      ]
-    }
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'media.test.miamibeachhotel.li'
+          }
+        ],
+        // This will cause the browser to redirect and show the new URL
+        destination: '/media/:path*',
+        permanent: false
+      }
+    ]
   }
 }
