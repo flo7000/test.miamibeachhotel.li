06df882d847125616e1ce7880825b6b8f99b44d9
flo7000 <95176997+flo7000@users.noreply.github.com>
2024-12-19 15:33:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,34 @@
 module.exports = {
   async rewrites() {
     return [
+      // For test2.domain.ch
+      {
+        source: '/',
+        destination: '/test2',
+        has: [
+          { type: 'host', value: 'test2.domain.ch' }
+        ]
+      },
       {
         source: '/:path*',
         destination: '/test2/:path*',
         has: [
-          {
-            type: 'host',
-            value: 'test2.miamibeachhotel.li'
-          }
+          { type: 'host', value: 'test2.domain.ch' }
+        ]
+      },
+      // For media.test.domain.ch
+      {
+        source: '/',
+        destination: '/media',
+        has: [
+          { type: 'host', value: 'media.test.domain.ch' }
         ]
       },
       {
         source: '/:path*',
         destination: '/media/:path*',
         has: [
-          {
-            type: 'host',
-            value: 'media.test.miamibeachhotel.li'
-          }
+          { type: 'host', value: 'media.test.domain.ch' }
         ]
       }
     ]
