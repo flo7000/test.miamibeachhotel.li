124a327eb1feb022f8a2126b7ae0a6dd4e7e264b
flo7000 <95176997+flo7000@users.noreply.github.com>
2024-12-19 16:57:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
-rewrites() {
-  return {
+module.exports = {
+  async rewrites() {
+    return {
       beforeFiles: [
-          // if the host is `app.acme.com`,
-          // this rewrite will be applied
-          {
-              source: '/:path*',
-              has: [
-                  {
-                      type: 'host',
-                      value: 'media.test.miamibeachhotel.li',
-                  },
-              ],
-              destination: '/media/:path*',
-          },
+        {
+          source: '/test-route',
+          destination: '/media'
+        }
       ]
+    }
   }
 }
